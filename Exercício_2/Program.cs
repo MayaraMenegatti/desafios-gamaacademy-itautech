2260899c03ee8f0ccfa2252be745587252a217c0
MayaraMenegatti <88911312+MayaraMenegatti@users.noreply.github.com>
2021-10-20 11:43:45 -0300

    Commit dos exercícios 1, 2, 3 e 4

---
+++ Exercício_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercício_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            string temp;
+
+            Console.Write("Introduza o primeiro número: ");
+            temp = Console.ReadLine();
+            a = Convert.ToInt32(temp);
+
+            Console.Write("Introduza o segundo número: ");
+            temp = Console.ReadLine();
+            b = Convert.ToInt32(temp);
+
+            Console.Write("Introduza o terceiro número: ");
+            temp = Console.ReadLine();
+            c = Convert.ToInt32(temp);
+
+        if (a > b) 
+        {
+            if (a > c) Console.WriteLine("O maior é " + a);
+            else Console.WriteLine("O maior é " + c);
+        }
+        else
+        {
+            if(b > c) Console.WriteLine("O maior é " + b);
+            else Console.WriteLine("O maior é " + c);
+        }
+        }
+    }
+}
---
+++ Exercício_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercício_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, p;
+            salario = Convert.ToDouble(Console.ReadLine());
+            if (salario <= 2800)
+            {
+                p = 20;
+            }
+            else if (salario <= 7000)
+            {
+                p = 15;
+            }
+            else if (salario <= 15000)
+            {
+                p = 10;
+            }
+
+            else
+            {
+                p = 5;
+            }
+
+            Console.WriteLine("salário original" + salario);
+            Console.WriteLine("porcentagem" + p);
+            p = p / 100;
+            double aumento = p * salario;
+            double novo = salario + aumento;
+            Console.WriteLine("aumento" + aumento);
+            Console.WriteLine("novo salario" + novo);
+        }
+    }
+}
